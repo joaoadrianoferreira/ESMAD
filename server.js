6f8d9a3e9b1326ac69444a6c7afd710239f75c81
João <joaoadrianoferreira@gmail.com>
2020-07-07 23:42:54 +0100

    graphql and apollo

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 require('dotenv').config();
 
-global.express = require('express');
-global.mongoose = require('mongoose');
-global.app = express();
+global.ApolloServer = require('apollo-server').ApolloServer;
 
-global.mongoose.connect('mongodb+srv://user:UD7PydkN3HvQ4Q87@cluster0-nqv05.mongodb.net/GraphQL_Boilerplate?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
-global.database_connection = mongoose.connection;
-global.database_connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-require('./imports');
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
 
-global.app.use("/persons/", require("./api/Persons/routes.js")); 
+// Some fake data
+const persons = [
+   {
+     name: "Adriano",
+     age: 33,
+   },
+   {
+     name: 'Carlos',
+     age: 23,
+   },
+ ];
 
-const server = app.listen(process.env.PORT || 3000, process.env.IP || "localhost", () => {
-   console.log("Server listening at http://%s:%s", server.address().address, server.address().port)
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { persons: [Person], books: [Book] }
+  type Book { title: String, author: String }
+  type Person { name: String, age: Int}
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books, persons: () => persons },
+};
+
+// Enable schema reporting in your Apollo Server constructor
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {    
+    reportSchema: true
+  }
+});
+
+apollo.listen(process.env.PORT || 3000, process.env.IP || "localhost", () => {
+   console.log("Server listening at http://%s:%s", process.env.IP || "localhost", process.env.PORT || 3000)
 })
